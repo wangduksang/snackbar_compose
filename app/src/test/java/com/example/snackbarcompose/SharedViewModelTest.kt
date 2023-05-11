aeb0192f59c3389014000b16518ff1c93dbb1018
wangduksang <wds8tem@gmail.com>
2023-05-11 18:43:10 +0900

    테스트 파일 생성

---
+++ app/src/test/java/com/example/snackbarcompose/SharedViewModelTest.kt
@@ -0,0 +1,28 @@
+package com.example.snackbarcompose
+
+import android.util.Log
+import com.example.snackbarcompose.data.models.Category
+import com.example.snackbarcompose.data.models.Item
+import com.example.snackbarcompose.data.models.constance.ItemDecorType
+import com.example.snackbarcompose.data.models.constance.Priority
+import com.google.gson.Gson
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import timber.log.Timber
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class SharedViewModelTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun `should return mock data - create local sample data`() {
+
+    }
+}
